8e1458af6645abc21a1cfd8bc799662f24728449
Eric Muli <44473888+EricRootLee@users.noreply.github.com>
2020-07-29 13:00:28 +0300

    Delete firebase.js
    Deleted DB config

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAxC1z4IiCaEQzKo2JU9IpSftV2srpJMXI",
-    authDomain: "instagram-clone-d99d6.firebaseapp.com",
-    databaseURL: "https://instagram-clone-d99d6.firebaseio.com",
-    projectId: "instagram-clone-d99d6",
-    storageBucket: "instagram-clone-d99d6.appspot.com",
-    messagingSenderId: "608600721575",
-    appId: "1:608600721575:web:3c05265eb94ffc75e73382"
-  };
